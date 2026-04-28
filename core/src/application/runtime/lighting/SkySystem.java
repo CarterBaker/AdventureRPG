0fe23d6ecba0c7642b39a816ba6b37fefc1dee87
Carter Baker <yourking77@gmail.com>
2026-04-27 23:16:57 -0400

    Checking in

--- core/src/application/runtime/lighting/SkySystem.java
+++ core/src/application/runtime/lighting/SkySystem.java
@@ -11,21 +11,29 @@ import engine.root.SystemPackage;
 
 public class SkySystem extends SystemPackage {
 
+    /*
+     * Submits the sky pass render call each frame and queues the sky FBO
+     * for compositing into the final scene.
+     */
+
+    // Internal
     private PassManager passManager;
     private RenderManager renderManager;
     private FboManager fboManager;
     private FboRenderSystem fboRenderSystem;
 
+    // Render Target
     private PassHandle skyPass;
     private FboInstance skyFbo;
 
     @Override
     protected void get() {
+
+        // Internal
         this.passManager = get(PassManager.class);
         this.renderManager = get(RenderManager.class);
         this.fboManager = get(FboManager.class);
         this.fboRenderSystem = get(FboRenderSystem.class);
-
     }
 
     @Override
--- core/src/application/runtime/world/WorldSystem.java
+++ core/src/application/runtime/world/WorldSystem.java
@@ -1,6 +1,7 @@
 package application.runtime.world;
 
 import application.bootstrap.entitypipeline.playermanager.PlayerManager;
+import application.bootstrap.renderpipeline.fbo.FboInstance;
 import application.bootstrap.renderpipeline.fbomanager.FboManager;
 import application.bootstrap.worldpipeline.worldstreammanager.WorldStreamManager;
 import engine.root.EngineSetting;
@@ -8,12 +9,23 @@ import engine.root.SystemPackage;
 
 public class WorldSystem extends SystemPackage {
 
+    /*
+     * Initializes the world stream grid for the context window at startup,
+     * binding the world render target so chunk rendering composites correctly.
+     */
+
+    // Internal
     private PlayerManager playerManager;
     private WorldStreamManager worldStreamManager;
     private FboManager fboManager;
 
+    // Render Target
+    private FboInstance worldFbo;
+
     @Override
     protected void get() {
+
+        // Internal
         this.playerManager = get(PlayerManager.class);
         this.worldStreamManager = get(WorldStreamManager.class);
         this.fboManager = get(FboManager.class);
@@ -21,10 +33,12 @@ public class WorldSystem extends SystemPackage {
 
     @Override
     protected void awake() {
+        this.worldFbo = fboManager.getFbo(EngineSetting.FBO_WORLD);
+
         int windowID = context.getWindow().getWindowID();
         worldStreamManager.createGrid(
                 playerManager.getPlayerForWindow(windowID),
                 context.getWindow(),
-                fboManager.getFbo(EngineSetting.FBO_WORLD));
+                worldFbo);
     }
 }
